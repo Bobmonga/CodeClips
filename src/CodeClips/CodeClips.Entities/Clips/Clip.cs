c97ffaced25883b117b0769095c85864fbf6eb84
HodgesI <ian.hodges@hddecisions.co.uk>
2018-07-06 10:52:13 +0100

    Add skeleton projects for Service and Entities.

---
+++ src/CodeClips/CodeClips.Entities/Clips/Clip.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CodeClips.Entities.Clips
+{
+    public class Clip
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public List<string> Tags { get; set; }
+    }
+}
---
+++ src/CodeClips/CodeClips/Controllers/ClipsController.cs
@@ -0,0 +1,54 @@
+﻿using CodeClips.Entities.Clips;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CodeClips.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClipsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            Clip clip = new Clip
+            {
+                Title = "REDIS Repo",
+                Content = "Here is some code",
+                Tags = new List<string>
+                {
+                    "C#",
+                    "REDIS",
+                    "Repository"
+                }
+            };
+
+            return new OkObjectResult(clip);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
