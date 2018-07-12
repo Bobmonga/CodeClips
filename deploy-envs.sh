da343e631b383872b98554de96f04683ac313077
Ian <doctorhodges@gmail.com>
2018-07-12 09:28:06 +0100

    Add files for Travis integration.

---
+++ deploy-envs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# set environment variables used in deploy.sh and AWS task-definition.json:
+export IMAGE_NAME=ianhodges-codeclips
+export IMAGE_VERSION=latest
+
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_ECS_CLUSTER_NAME=default
+export AWS_VIRTUAL_HOST=ec2-34-243-54-31.eu-west-1.compute.amazonaws.com
+
+# set any sensitive information in travis-ci encrypted project settings:
+# required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+# optional: SERVICESTACK_LICENSE
